8cb8d69cafa686d52000e331eefe02d35ddb8273
shour0 <shour908@gmail.com>
2025-06-03 18:07:41 +0300

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px10 px-5">
+      <div className="max-w-7xl w-full">
+        <h1 className="">hello, Portofolio</h1>
+      </div>
+    </main>
+  );
+}
