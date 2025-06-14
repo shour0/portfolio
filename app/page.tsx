7db8843fadd103012b1777e91c87f2e4523d7ad1
shour0 <shour908@gmail.com>
2025-06-14 12:58:34 +0300

    made navbar funtionable

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProject from "@/components/RecentProject";
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
 import { FaHome } from "react-icons/fa";
 
 export default function Home() {
@@ -9,11 +10,7 @@ export default function Home() {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px10 px-5">
       
       <div className="max-w-[120rem] w-full">
-        <FloatingNav
-         navItems={[
-          {name: 'Home', link:'/', icon: <FaHome />}
-         ]}
-         /> 
+        <FloatingNav navItems={navItems}/> 
         <Hero 
         />
         
