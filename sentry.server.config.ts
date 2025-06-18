8f3c40fcdbdab777ecb9fc5d5555743bbf534f51
shour0 <shour908@gmail.com>
2025-06-18 18:35:26 +0300

    Fixed sentry issues

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,12 +5,12 @@
 import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: "https://98907470de80cd06913d299e25361de5@o4509174675603456.ingest.de.sentry.io/4509496458805328",
-  integrations: [
-    Sentry.feedbackIntegration({
-      // Additional SDK configuration goes in here, for example:
-      colorScheme: "dark",
-    }),
-  ],
+  // integrations: [
+  //   Sentry.feedbackIntegration({
+  //     // Additional SDK configuration goes in here, for example:
+  //     colorScheme: "dark",
+  //   }),
+  // ],
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
