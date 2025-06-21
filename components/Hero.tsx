23c7a8f44d3b7e55494a52f133a16f12e25428e8
shour0 <shour908@gmail.com>
2025-06-21 16:41:57 +0300

    FIxed Hero section Padding on top

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ import { SparklesCore } from './ui/Sparkles'
 
 const Hero = () => {
   return (
-    <div className='pb-20 pt-36 '>
+    <div className='pb-20 pt-28 '>
       
       <div>
         <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white' />
