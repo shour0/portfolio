842562d27ead07c4779f1714b5af18b1cf667e63
shour0 <shour908@gmail.com>
2025-06-21 16:59:12 +0300

    fixed padding top on small deviced in hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ import { SparklesCore } from './ui/Sparkles'
 
 const Hero = () => {
   return (
-    <div className='pb-20 pt-28 '>
+    <div className='pb-20 lg:pt-36 sm:pt-0 '>
       
       <div>
         <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white' />
